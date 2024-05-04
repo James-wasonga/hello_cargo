0dbd495b97fb81b1dfe96714988bf33446dd336c
James-wasonga <jameswasonga22@gmail.com>
2024-05-04 20:51:17 +0300

    rust cohort

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+fn main(){
+    //scalar dataypes
+    
+    //boolean
+    //number
+    //character
+    //integer
+    //float
+    
+    
+    
+    //signed && unsigned
+    
+    //integer
+    // let number1 :i8 = 10;
+    // let number2: u8 = 33;
+    
+    // let number3: i32 = 40;
+    // let number3: u32 = 43;
+    
+    // let number4: i64 = 50;
+    // let number5: u64 = 63;
+    
+    // let number6: i128 = 128;
+    // let number7: u128 = 129;
+    // println!("The number is {}", number1);
+
+    //float
+    // let number :f32 = 111.23;
+    // let number :f32 = 110.23;
+
+    //character
+    // let character : char = 't';
+    // println!("The number is {}", character);
+
+    // //string
+    // const text: &str = "Rust keeps me moving";
+    // let text1: String = String::from("Hello");
+
+
+    //  println!("{}",text);
+    // println!("{}",text1);
+
+sum(2,4);
+sums(3,8);
+suma(4,6);
+
+
+
+}
+fn sum(number_1: i32, number_2: i32){
+    let number_3: i32 = number_1 + number_2;
+    println!("sum of 1 and 2 {}", number_3);
+}
+
+fn sums(number_1: i32, number_2: i32) -> i32{
+    let number_3: i32 = number_1 + number_2;
+    println!("sum of 1 and 2 {}", number_3);
+    return number_3;
+}
+fn suma(number_1: i32, number_2: i32)-> i32{
+    let number_3: i32 = number_1 + number_2;
+    println!("sum of 1 and 2 {}", number_3);
+    number_3
+}
+    
+   
