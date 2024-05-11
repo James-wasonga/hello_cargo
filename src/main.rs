020a0be6bc3e818b7f9b780d046a1ea60b8345ce
James-wasonga <jameswasonga22@gmail.com>
2024-05-11 14:47:15 +0300

    control flows

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,95 @@
-fn main(){
-    //scalar dataypes
-    
-    //boolean
-    //number
-    //character
-    //integer
-    //float
-    
-    
-    
-    //signed && unsigned
-    
-    //integer
-    // let number1 :i8 = 10;
-    // let number2: u8 = 33;
-    
-    // let number3: i32 = 40;
-    // let number3: u32 = 43;
-    
-    // let number4: i64 = 50;
-    // let number5: u64 = 63;
-    
-    // let number6: i128 = 128;
-    // let number7: u128 = 129;
-    // println!("The number is {}", number1);
-
-    //float
-    // let number :f32 = 111.23;
-    // let number :f32 = 110.23;
-
-    //character
-    // let character : char = 't';
-    // println!("The number is {}", character);
-
-    // //string
-    // const text: &str = "Rust keeps me moving";
-    // let text1: String = String::from("Hello");
 
 
-    //  println!("{}",text);
-    // println!("{}",text1);
+//  fn main() {
+//     let mut s: String = String::from(s:"Hello");
 
-// sum(2,4);
-// sums(3,8);
-// suma(4,6);
+//     change(&mut s);
+//     println!("->{s}")
+//  }
 
+//  fn change(some_string: &mut String){
+//     some_string.push_str(string:",world");
+//  }
+
+// fn main(){
+//    let _s2 :String = String::from("hello"); 
+//  let _s1 = give_ownership();
+//  let _s3 = take_and_gives_back(_s2);
 
 
 // }
-// fn sum(number_1: i32, number_2: i32){
-//     let number_3: i32 = number_1 + number_2;
-//     println!("sum of 1 and 2 {}", number_3);
+
+// fn give_ownership() -> String{
+//    let some_string = String::from("your");
+//    some_string
 // }
 
-// fn sums(number_1: i32, number_2: i32) -> i32{
-//     let number_3: i32 = number_1 + number_2;
-//     println!("sum of 1 and 2 {}", number_3);
-//     return number_3;
+// fn take_and_gives_back(a_string: String) -> String {
+
+//    a_string
 // }
-// fn suma(number_1: i32, number_2: i32)-> i32{
-//     let number_3: i32 = number_1 + number_2;
-//     println!("sum of 1 and 2 {}", number_3);
-//     number_3
+
+
+// fn main(){
+//    let s1 = String::from("hello");
+//    let (s2, len) = calculate_length(s1.clone());
+//    println!("length of {} is {}",s1,len);
+
 // }
-    
- //Making test
 
- let mut m = String::from("I have done my exams"); 
- //adding a string literal 
+// fn calculate_length(s: String) -> (String,usize){
+//    let length = s.len();
+//    (s, length)
 
- m.push_str(" for this semester");
-
- let s = m.clone();
- println!("m is {} and s is {}",m,s);
- println!("{}",m); 
- }
+// }
 
 
+//reference 
+// fn main(){
+//    let s1 = String::from("Hello");
+//    let len = calculate_length(&s1);
+//    println!("The length of '{}' is {}.", s1, len);
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
 
 
+// }
 
 
+// fn main(){
+// let network_response:i32 = 200;
+// let is_success: bool = if network_response == 200{true} else{false};
+
+// if is_success {
+//    println!("Operation successfull")
+// }
+
+//    let mut counter :i32 = 0;
+//    loop{
+//       counter = counter + 1;
+//       println!("I am at num {counter}");
+
+//       if counter == 1 {
+//          println!("Starting point")
+//       }
+//       if counter == 10 {
+//          break
+//       }
+//    }
+// }
 
 
+fn main() {
+ let mut element = [1,2,3,4,5,6,7,8,9];
+//  let mut index = 5;
 
- 
+//  while index != 0{
+//    println!("looping through index {index} item in array{}",array[index]);
+//    index -=1;
+//  }
+for element in element{
+   println!("The value is: {element}");
+}
+}
