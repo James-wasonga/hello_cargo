1c7a33cddf442489b3343c656dc893e05b2ca41d
James-wasonga <jameswasonga22@gmail.com>
2024-05-11 18:28:48 +0300

    for loop

--- src/main.rs
+++ src/main.rs
@@ -160,21 +160,36 @@
 // }
 
 //using loop
-use std::io;
+// use std::io;
 
-fn main() {
-    println!("Enter an integer:");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read the value in the line");
+// fn main() {
+//     println!("Enter an integer:");
+//     let mut input = String::new();
+//     io::stdin().read_line(&mut input).expect("Failed to read the value in the line");
 
-    let num: i32 = input.trim().parse().expect("Please enter a valid integer");
+//     let num: i32 = input.trim().parse().expect("Please enter a valid integer");
 
-    let mut i = 0;
-    loop {
-        if i > num {
-            break;
-        }
-        println!("{}", i);
-        i += 1;
-    }
+//     let mut i = 0;
+//     loop {
+//         if i > num {
+//             break;
+//         }
+//         println!("{}", i);
+//         i += 1;
+//     }
+// }
+ use std::io;
+
+fn main(){
+  println!("Enter an integer");
+  
+  let mut input = String::new();
+  io::stdin().read_line(&mut input).expect("Failed to read the value");
+
+  let num: i32 = input.trim().parse().expect("Please enter a valid input");
+   for i in 0.. num {
+      println!("{}", i);
+   }
+
 }
+
